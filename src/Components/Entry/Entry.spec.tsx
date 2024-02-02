24293546d9998541c2b6b2e0b46593a630b56e20
Thiago Melo <thiago.tmwd@gmail.com>
2024-02-02 10:28:57 -0300

    fix: linting

--- src/Components/Entry/Entry.spec.tsx
+++ src/Components/Entry/Entry.spec.tsx
@@ -5,7 +5,7 @@ import { Entry } from './Entry';
 import { act, render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-jest.mock('../../service/NotifySystem.ts')
+jest.mock('../../service/NotifySystem.ts');
 
 test('Should render component <Entry />', async () => {
     await act(async () => {
