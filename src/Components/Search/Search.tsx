927a2109989b47c7ea787951d19e409c9811bbc3
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-06 14:30:06 -0300

    fix: autocomplete

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,15 +1,15 @@
 import React, { KeyboardEventHandler } from 'react';
 interface SearchProps {
-    search: KeyboardEventHandler<HTMLInputElement> | undefined;
+    search?: KeyboardEventHandler<HTMLInputElement> | any;
 }
 
 export const Search: React.FC<SearchProps> = ({ search }): JSX.Element => {
-    const SearchProps = {
+    const inputProps: React.InputHTMLAttributes<HTMLInputElement> = {
         type: 'text',
         onKeyUp: search,
         className: 'form-control form-control-lg mb-4',
         placeholder: 'Digite o nome de um estado aqui...',
     };
 
-    return <input {...SearchProps} />;
+    return <input {...inputProps} />;
 };
