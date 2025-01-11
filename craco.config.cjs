caa8a9b4aacc102387925026d08a782d83a10848
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-11 09:14:12 -0300

    feat: ajusts

--- craco.config.cjs
+++ craco.config.cjs
@@ -3,12 +3,18 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
+      
+      webpackConfig.output.publicPath =
+        env === "production"
+          ? "https://tjmelo.github.io/tmx-mercury/" // Production CDN URL
+          : "http://localhost:3000/tmx-mercury/"; 
+
       webpackConfig.plugins.push(
         new ModuleFederationPlugin({
           name: 'TmxMercury',
           filename: 'remoteEntry.js',
           exposes: {
-            './TmxMercury': './src/index',
+            './index': './src/index.tsx',
           },
           shared: {
             react: { singleton: true, eager: true },
