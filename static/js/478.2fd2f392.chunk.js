7d403b993debc87bace1a9969963a6f9a4cbda9c
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-11 13:40:56 -0300

    Updates

--- static/js/478.2fd2f392.chunk.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktmx_mercury=self.webpackChunktmx_mercury||[]).push([[478],{478:(e,m,r)=>{Promise.all([r.e(664),r.e(438)]).then(r.bind(r,720))}}]);
-//# sourceMappingURL=478.2fd2f392.chunk.js.map
